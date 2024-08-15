8e4dc36f344351ea6c8aa9da6fd6c2698892dd32
Nerdzter <nayderson.silva.oliveira@gmail.com>
2024-08-14 22:31:26 -0300

    Commit #3 - Aula

---
+++ primeiro-projeto/src/MyClass.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    
+    public static void main(String[] args) {
+        // escopo Main
+        
+       // int age = 15;
+
+       // String country = "Brasil";
+
+        System.out.println("Eu sou o Nayderson, e eu venho do");
+    }
+}
